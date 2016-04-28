3bc0eaafc7296dcd6397e3c9152d5c8873db4e75
Ivan Scherbak <dev@funivan.com>
2016-04-28 10:18:18 +0300

    Add line ending review

---
+++ Tests/ReviewTestCase.php
@@ -0,0 +1,48 @@
+<?php
+
+  namespace Tests\Funivan\Cs;
+
+  use Funivan\Cs\FileFinder\FileInfo;
+  use Funivan\Cs\FileProcessor\FileTool;
+  use Funivan\Cs\Message\Report;
+
+  /**
+   *
+   */
+  abstract class ReviewTestCase extends \PHPUnit_Framework_TestCase {
+
+    /**
+     * @return FileTool
+     */
+    public abstract function getTool();
+
+
+    /**
+     * @param string $input
+     * @return Report
+     */
+    protected function process($input) {
+      $path = tempnam(sys_get_temp_dir(), 'review-test');
+      file_put_contents($path, $input);
+
+      $file = new FileInfo($path, FileInfo::STATUS_UNKNOWN);
+      $tokenizer = $file->getTokenizer();
+      unlink($path);
+
+      $this->assertNotEmpty($tokenizer);
+
+      $tool = $this->getTool();
+
+
+      $report = new Report();
+      $tool->process($file, $report);
+
+
+      $output = $tokenizer->getCollection()->assemble();
+
+      $this->assertEquals($input, $output, 'Review should not change source code. Tool:' . $tool->getName());
+
+      return $report;
+    }
+
+  }
---
+++ src/Tools/LineEnding/Tests/LineEndingReviewTest.php
@@ -0,0 +1,76 @@
+<?php
+
+  namespace Funivan\Cs\Tools\Tests\LineEnding;
+
+  use Funivan\Cs\FileProcessor\FileTool;
+  use Funivan\Cs\Tools\LineEnding\LineEndingReview;
+  use Tests\Funivan\Cs\ReviewTestCase;
+
+  /**
+   *
+   */
+  class LineEndingReviewTest extends ReviewTestCase {
+
+    /**
+     * @return FileTool
+     */
+    public function getTool() {
+      return new LineEndingReview();
+    }
+
+
+    /**
+     * @return array
+     */
+    public function getReplaceSpacesDataProvider() {
+      return [
+        [
+          'code' => '<?php\n 
+  echo 1;\r',
+          'lines' => [2],
+        ],
+        [
+          'code' => '<?php echo 1;',
+          'lines' => [],
+        ],
+        [
+          'code' => '<?php\r\n
+          echo 1;\n
+          \r',
+          'lines' => [1, 2],
+        ],
+        [
+          'code' => '<?php\n
+\r\n
+\r\n',
+          'lines' => [2],
+
+        ],
+      ];
+    }
+
+
+    /**
+     * @dataProvider getReplaceSpacesDataProvider
+     * @param string $input
+     * @param array $expectErrorLines
+     */
+    public function testReviewInvalidLines($input, array $expectErrorLines) {
+      $map = [
+        '\r' => "\r",
+        '\n' => "\n",
+      ];
+
+      $input = str_replace("\n", '', $input);
+      $input = strtr($input, $map);
+
+      $report = $this->process($input);
+      $errorLines = [];
+      foreach ($report as $message) {
+        $errorLines[] = $message->getLine();
+      }
+
+      $this->assertEquals($expectErrorLines, $errorLines);
+    }
+
+  }
